8d789c83933a5457c72e3f79610a9aec3f6feba5
NumtraAdmin <NumtraAdmin>
2020-01-10 13:28:16 +0000

    2.6*AutoFE_10%_DesignChanges

--- job5db2b88f7267f58cb8bf3995.py
+++ job5db2b88f7267f58cb8bf3995.py
@@ -1,9 +1,12 @@
-from azureml.core.authentication import ServicePrincipalAuthentication
-from azureml.core.webservice import AciWebservice, Webservice
-from azureml.core import Workspace
 import traceback
 import sys
+from operations import TopOperation
+from operations import JoinOperation
+from operations import AggregationOperation
+from operations import FormulaOperation
+from operations import FilterOperation
 from connectors import DBFSConnector
+from connectors import CosmosDBConnector
 from datatransformations import TranformationsMainFlow
 from automl import tpot_execution
 from core import PipelineNotification
@@ -27,7 +30,7 @@ except Exception as ex:
 	sys.exit(1)
 try: 
 	PipelineNotification.PipelineNotification().started_notification('5db2b88f7267f58cb8bf3998','567a95c8ca676c1d07d5e3e7','http://104.40.91.74:3200/pipeline/notify')
-	Movie_Recommendation_AutoML = tpot_execution.Tpot_execution.run(["5db2b88f7267f58cb8bf3997"],{"5db2b88f7267f58cb8bf3997": Movie_Recc_FE}, "5db2b88f7267f58cb8bf3998", spark,json.dumps( {"model_type": "regression", "label": "AvgRating", "run_id": "13db06c3acb9461da552290acd458508", "features": ["UserId", "MovieId", "Rating", "Timestamp", "Timestamp_dayofmonth", "Timestamp_month", "Timestamp_year"], "percentage": "50", "executionTime": 15, "sampling": "0", "sampling_value": "", "model_id": "5e04bde51bfdaec91f2f9ca3", "ProjectName": "Retail Scenarios", "PipelineName": "MoviePredictionSystem", "userid": "567a95c8ca676c1d07d5e3e7", "runid": "13db06c3acb9461da552290acd458508", "url_ResultView": "http://104.40.91.74:3200", "experiment_id": "895518857185768"}))
+	Movie_Recommendation_AutoML = tpot_execution.Tpot_execution.run(["5db2b88f7267f58cb8bf3997"],{"5db2b88f7267f58cb8bf3997": Movie_Recc_FE}, "5db2b88f7267f58cb8bf3998", spark,json.dumps( {"model_type": "regression", "label": "AvgRating", "run_id": "", "features": ["UserId", "MovieId", "Rating", "Timestamp", "Timestamp_dayofmonth", "Timestamp_month", "Timestamp_year"], "percentage": "10", "executionTime": 3, "sampling": "0", "sampling_value": "", "model_id": "5dfa2b071bfdaec91f2ce17b", "ProjectName": "Retail Scenarios", "PipelineName": "MoviePredictionSystem", "userid": "567a95c8ca676c1d07d5e3e7", "runid": "", "url_ResultView": "http://104.40.91.74:3200", "experiment_id": "895518857185768"}))
 
 	PipelineNotification.PipelineNotification().completed_notification('5db2b88f7267f58cb8bf3998','567a95c8ca676c1d07d5e3e7','http://104.40.91.74:3200/pipeline/notify')
 except Exception as ex: 
